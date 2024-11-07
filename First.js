20b682fe6763431b995932d72bca9f5a8c0f59e6
Areeby-Khan <areebalouch@gmail.com>
2024-11-07 18:17:30 +0500

    Add files via upload

---
+++ First.js
@@ -0,0 +1,25 @@
+
+const product = {
+     title : "Parker Jotter Standard CT ball Pen ",
+    rating : 4,
+    offer : 5, 
+    price : 300,
+}; 
+console.log(product);
+
+const profile = {
+    userName : "Shradha Khapra ",
+    isFollow : true,
+    isMessage : true , 
+    followers : 500, 
+    following : 4, 
+    totalPost : 195 , 
+    about : "ex-Microsoft , DRDO "
+};
+console.log(profile);
+
+
+
+// prompt
+   let input = prompt("Enter a Number");
+   console.log(input);
---
+++ variables.js
@@ -0,0 +1,72 @@
+console.log(" JAVASCRIPT");
+    // constant declare 
+   const accountId = 1234;
+   let accountEmail = "areebalouch@gmail.com";
+   var accountPassword = " 123445";
+   accountCity = "Bahawalpur ";
+   console.log(accountCity);
+   console.table([ accountId , accountEmail , accountPassword , accountCity]);
+
+
+
+   //DATA TYPE 
+   "use strict ";     // treat all js code as newer version 
+  // alert(3+4); // we are using node js , not brower 
+   
+    // conversion 
+let score = "33";  
+    console.log(typeof score ); 
+    console.log(typeof (score));
+
+    let valueOfNumber = Number(score);
+    console.log(typeof valueOfNumber);
+    console.log(valueOfNumber);
+
+    let eng="nineth";
+    console.log(typeof eng);
+     let subject = Number(typeof eng);
+     console.log(subject); 
+
+
+     let juice= true ;
+     console.log(typeof juice);
+     console.log(typeof (juice));
+
+     let market = Number(typeof juice);
+     console.log(typeof market);  
+
+
+
+     // conversionnnn 
+
+    //   let math = undefined;
+    //   console.log(typeof math);
+    //   console.log(typeof (math));
+
+    //   let valueOfMath = Number (typeof math);
+    //   console.log(typeof valueOfMath);
+    //   console.log(valueOfMath);
+     
+
+    //    let isLoggedIn = 1;
+
+    //    let booleanIsLoggedIn = Boolean (isLoggedIn);
+    //    console.log(booleanIsLoggedIn);
+
+    //    let urdu = 0 ;
+    //    let isPassed = Boolean (urdu);
+    //    console.log(isPassed); 
+
+
+    //    let pipa = 1;
+    //     let isMyFrnd = Boolean (pipa);
+    //     console.log(isMyFrnd);
+
+
+    // let irum = " is my best friend";
+    //  let isRealFriend = Number(irum);
+    //  console.log(isRealFriend);
+
+    let age = 20; 
+     let isMyAge = Boolean (age);
+     console.log(isMyAge); 
